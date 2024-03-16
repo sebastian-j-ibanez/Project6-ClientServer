85324c035544bf8813224b650bc76ad630e59610
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-03-16 14:13:11 -0400

    Add WriteToFile helper function in FileIO.hpp. Add planeData struct.

--- Project6-Server/PlaneData.hpp
+++ Project6-Server/PlaneData.hpp
@@ -1,6 +1,21 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
+
 struct planeData {
 	float fuelCompAvg;
 	int numTrans;
+
+    // Return formatted string with PlaneData fields.
+    std::string ToString() {
+        std::stringstream ss;
+        ss << "Fuel Consumption Average:" << fuelCompAvg << std::endl << "Number of transmissions:" << numTrans << std::endl;
+        return ss.str();
+    }
+
+    // Print PlaneData to std::cout.
+    void Print() {
+        std::cout << ToString() << std::endl;
+    }
 };
