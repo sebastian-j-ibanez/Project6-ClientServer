679ac54e439fe8865c0805075984025eb4dbe194
Cosmin Ene <nationalsown@gmail.com>
2024-03-17 17:22:14 -0400

    Create ClientGroupStart.bat

---
+++ ClientGroupStart.bat
@@ -0,0 +1,19 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM Prompt the user for input
+SET /P userInput=How many planes to take off?: 
+
+REM Display the user's input
+ECHO Number of planes taking off: %userInput%
+
+SET UniqueID=1
+
+REM Execute a command for the specified number of iterations
+FOR /L %%x IN (1, 1, %userInput%) DO (
+	ECHO Iteration %%x
+	
+	"client.exe" "%UniqueID%"
+)
+
+SET /P endMenu=Press the enter key to end program: 
