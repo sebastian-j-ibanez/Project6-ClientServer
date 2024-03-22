a4428ea4b99c0cd866798797ebde62cd4c5d43cf
Cosmin <nationalsown@gmail.com>
2024-03-22 08:19:39 -0400

    Messing with bat file

--- ClientGroupStart.bat
+++ ClientGroupStart.bat
@@ -8,10 +8,13 @@ REM Display the user's input
 ECHO Number of planes taking off: %userInput%
 
 REM Execute a command for the specified number of iterations
-FOR /L %%x IN (1, 1, %userInput%) DO (
+FOR /L %%x IN (1, 1, %userInput%) DO 
+(
 	ECHO Iteration %%x
 	
-	START %~dp0\x64\Debug\Project6-Client.exe --%%x
+	START "" "%~dp0\x64\Debug\Project6-Client.exe" -%%x 
 )
+
+
 
 SET /P endMenu=Press the enter key to end program: 
