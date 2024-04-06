e46669da4fbd2cbe16dd1eae77e97fc884f6064d
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2024-04-06 13:45:26 -0400

    Updated batch script.

--- ClientGroupStart.bat
+++ ClientGroupStart.bat
@@ -7,11 +7,16 @@ SET /P userInput=How many planes to take off?:
 REM Display the user's input
 ECHO Number of planes taking off: %userInput%
 
+SET /P startingId=What is your starting Id?:
+ECHO What ID would you like to start at: %startingId%
+
+SET /A max=%userInput%+%startingId%
+
 REM Execute a command for the specified number of iterations
-FOR /L %%x IN (1, 1, %userInput%) DO (
+FOR /L %%x IN (%startingId%, 1, %max%) DO (
 	ECHO Iteration %%x
 	
-	START %~dp0\x64\Debug\Project6-Client.exe %%x
+	START %~dp0\x64\Release\Project6-Client.exe %%x
 )
 
 SET /P endMenu=Press the enter key to end program: 
